a87b4fc03e2037a55c96b3bf56da2c397058b4d7
Michael Pilosov <40366263+mathematicalmichael@users.noreply.github.com>
2024-12-09 00:43:40 +0000

    remove torch as core dep for now, assert in tests

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,19 +1,18 @@
-from parampacmap import ParamPaCMAP
-
 import numpy as np
+
+from parampacmap import ParamPaCMAP
 
 
 def main():
-
-    A = np.random.randn(1000, 20)
-
+    A = np.random.randn(100, 20)
     r = ParamPaCMAP(num_workers=0).fit_transform(A)
-
-    print(r)
+    assert r.shape[0] == A.shape[0]
+    assert r.shape[1] == 2
 
 
 def test_basic_usage():
     main()
+
 
 if __name__ == "__main__":
     main()
